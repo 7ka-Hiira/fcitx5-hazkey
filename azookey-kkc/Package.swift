8cc15778201fb82b1984dcc6bdb7f57a0c829adb
Nanaka Hiira <hiira@hiira.dev>
2024-07-08 22:50:08 +0900

    update AKKKC

--- azookey-kkc/Package.swift
+++ azookey-kkc/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/7ka-Hiira/AzooKeyKanaKanjiConverter",
-      branch: "848f99b")
+      branch: "658de4a")
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
