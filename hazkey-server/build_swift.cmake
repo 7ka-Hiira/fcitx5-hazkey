890a2df73eca9c55c453af1a5b7753a9d9c87651
Nanaka Hiira <hiira@hiira.dev>
2025-10-26 10:11:55 +0900

    feat: auto detect swift lib path

--- hazkey-server/build_swift.cmake
+++ hazkey-server/build_swift.cmake
@@ -1,18 +1,21 @@
 set(SWIFT_BUILD_TYPE "${SWIFT_BUILD_TYPE}")
 set(SWIFT_EXECUTABLE "${SWIFT_EXECUTABLE}")
+set(SWIFT_DETECTED_LIB_PATH "${SWIFT_DETECTED_LIB_PATH}")
 set(SWIFT_LINK_PATH "${SWIFT_LINK_PATH}")
 set(SWIFT_WORK_DIR "${SWIFT_WORK_DIR}")
 set(LLAMA_STUB_DIR "${LLAMA_STUB_DIR}")
+
 
 set(SWIFT_COMMAND
     ${SWIFT_EXECUTABLE} build -c ${SWIFT_BUILD_TYPE}
     --scratch-path=${CMAKE_CURRENT_BINARY_DIR}/swift-build
     -Xswiftc -static-stdlib
     -Xlinker -L${LLAMA_STUB_DIR}
-    -Xlinker -L/usr/lib/swift/linux
-    -Xlinker -L/usr/lib/swift/lib/swift/linux
-    -Xlinker -L/usr/local/lib/swift/linux
 )
+
+if(SWIFT_DETECTED_LIB_PATH)
+    list(APPEND SWIFT_COMMAND -Xlinker -L${SWIFT_DETECTED_LIB_PATH})
+endif()
 
 if(SWIFT_LINK_PATH)
     list(APPEND SWIFT_COMMAND -Xlinker -L${SWIFT_LINK_PATH})
